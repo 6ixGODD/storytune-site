b43b2a4bcddf39c6149aac603668192e1cad4dd6
BoChen SHEN <6goddddddd@gmail.com>
2026-05-12 15:06:58 +0800

    feat: add robots.txt generation for SEO opt

---
+++ app/robots.ts
@@ -0,0 +1,10 @@
+import { MetadataRoute } from 'next';
+
+import { config } from '@/lib/config';
+
+export default function robots(): MetadataRoute.Robots {
+    return {
+        rules: { userAgent: '*', allow: '/', disallow: '/admin/' },
+        sitemap: `${config.app.baseUrl}/sitemap.xml`,
+    };
+}
